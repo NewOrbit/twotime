987434275154d2f61031ee25642b7b6ea6506b8b
James Monger <jameskmonger@hotmail.co.uk>
2018-09-05 12:00:27 +0100

    Add TargetProcess config

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const harvest = {
+    accessToken: "",
+    accountId: 0
+};
+
+export const targetProcess = {
+    username: "",
+    password: ""
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { HarvestApi } from "./harvest/api";
 
-import { accessToken, accountId } from "./config";
+import { harvest } from "./config";
 
-const api = new HarvestApi(accessToken, accountId);
+const api = new HarvestApi(harvest.accessToken, harvest.accountId);
 api.getProjects().then(p => {
 	api.startTimeEntry(p[0].id, p[0].tasks[0].id, "2018-09-05", "testing twotime")
 		.then(entry => {
