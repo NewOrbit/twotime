cc8e25a9edb90ef263966f55703f6cf41f408f4b
James Monger <jameskmonger@hotmail.co.uk>
2018-10-22 15:45:35 +0100

    Respect the issue time option on Targetprocess (#12)

--- src/commands/finish.ts
+++ src/commands/finish.ts
@@ -22,7 +22,34 @@ const stopHarvestTimer = (harvestApi: HarvestApi, timeEntry: HarvestTimeEntry) =
     return Promise.all(actions);
 };
 
-const updateTargetProcess = (targetprocessApi: Targetprocess, tpEntity: any, timeEntry: HarvestTimeEntry, timeRemaining: number) => {
+const getTargetprocessTimeEntity = async (targetprocessApi: Targetprocess, tpEntity: any) => {
+    // if it's not a bug, we always log the time directly
+    if (tpEntity.ResourceType !== "Bug") {
+        return tpEntity;
+    }
+
+    const issueTimeTo = await targetprocessApi.getCustomValueForProject(tpEntity.Project.Id, "IssueTime to");
+
+    if (issueTimeTo === "none") {
+        log.info(`Project ${tpEntity.Project.Name} (${tpEntity.Project.Id}) is not configured to log issue time`);
+        return null;
+    }
+
+    if (issueTimeTo === "User story") {
+        log.info(`Project ${tpEntity.Project.Name} (${tpEntity.Project.Id}) is configured to log issue time to the user story`);
+        return tpEntity.UserStory;
+    }
+    
+    return tpEntity;
+};
+
+const updateTargetprocess = (targetprocessApi: Targetprocess, tpEntity: any, timeEntry: HarvestTimeEntry, timeRemaining: number) => {
+    const timeEntity = getTargetprocessTimeEntity(targetprocessApi, tpEntity);
+
+    if (timeEntity === null) {
+        return;
+    }
+
     return targetprocessApi.addTime(tpEntity.Id, timeEntry.hours, timeRemaining, new Date(timeEntry.created), "-");
 };
 
@@ -38,7 +65,7 @@ export const finish = async (apiProvider: ApiProvider) => {
     const targetprocessApi = apiProvider.getTargetprocessApi();
 
     log.info("Updating Targetprocess...");
-    await updateTargetProcess(targetprocessApi, details.tpEntity, details.timeEntry, details.timeRemaining);
+    await updateTargetprocess(targetprocessApi, details.tpEntity, details.timeEntry, details.timeRemaining);
 
     log.info("Updating harvest...");
     await stopHarvestTimer(harvestApi, details.timeEntry);
