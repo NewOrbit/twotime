08ffda9e18fa2c8a4843d09268b20f6c478bcdbe
James Monger <jameskmonger@hotmail.co.uk>
2018-10-23 11:23:40 +0100

    Implement parseDateOption

--- src/utils/parse-date-option.ts
+++ src/utils/parse-date-option.ts
@@ -1,3 +1,11 @@
+import * as moment from "moment";
+
+const isValidDate = (date: string) => moment(date, "YYYY-MM-DD", true).isValid();
+
 export const parseDateOption = (getTodayDate: () => string, date: string) => {
-    return "";
+    if (date === null || date === undefined || isValidDate(date) === false) {
+        return getTodayDate();
+    }
+
+    return date;
 };
