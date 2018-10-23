c9299f2f3e9e1ea64880ebc1c682b1bfc72674df
James Monger <jameskmonger@hotmail.co.uk>
2018-10-23 12:29:48 +0100

    Allow user to work with timers in the past (#18)
    * Add tests for parseDateOption
    
    * Implement parseDateOption
    
    * Pass date option to finish and start commands
    
    * Add README
    
    * Throw error on invalid date

--- src/register-commands.ts
+++ src/register-commands.ts
@@ -4,17 +4,52 @@ import { ApiProvider } from "./api-provider";
 import { start } from "./commands/start";
 import { finish } from "./commands/finish";
 import { auth } from "./commands/auth";
+import { isValidDate } from "./utils/is-valid-date";
+import { getTodayDate } from "./utils/get-today-date";
+import { log } from "./utils/log";
+
+const getDateForCommand = (command) => {
+    if (command.date === undefined || command.date === null) {
+        return getTodayDate();
+    }
+
+    if (isValidDate(command.date)) {
+        return command.date;
+    }
+
+    return null;
+};
 
 export const registerCommands = (commander: CommanderStatic, apiProvider: ApiProvider) => {
     commander
         .command("start")
         .description("start a timer")
-        .action(() => start(apiProvider));
+        .option("-d, --date <date>", "specify a date in YYYY-MM-DD format")
+        .action((cmd) => {
+            const date = getDateForCommand(cmd);
+
+            if (date === null) {
+                log.error("Invalid date provided. Use YYYY-MM-DD");
+                process.exit(1);
+            }
+
+            return start(apiProvider, date);
+        });
 
     commander
         .command("finish")
         .description("finish a timer")
-        .action(() => finish(apiProvider));
+        .option("-d, --date <date>", "specify a date in YYYY-MM-DD format")
+        .action((cmd) => {
+            const date = getDateForCommand(cmd);
+
+            if (date === null) {
+                log.error("Invalid date provided. Use YYYY-MM-DD");
+                process.exit(1);
+            }
+
+            return finish(apiProvider, date);
+        });
 
     commander
         .command("auth")
---
+++ test/is-valid-date.test.ts
@@ -0,0 +1,30 @@
+import { TestFixture, TestCase, Expect } from "alsatian";
+import { isValidDate } from "../src/utils/is-valid-date";
+
+const MOCK_TODAY_DATE_RESULT = "1996-01-01";
+const MOCK_GET_TODAY_DATE = () => MOCK_TODAY_DATE_RESULT;
+
+@TestFixture()
+export class IsValidDateTests {
+
+    @TestCase("2018-01-01")
+    @TestCase("2015-12-08")
+    public shouldReturnTrueWhenValidDateProvided(providedDate: string) {
+        const result = isValidDate(providedDate);
+
+        Expect(result).toBe(true);
+    }
+
+    @TestCase(undefined)
+    @TestCase(null)
+    @TestCase("2018-01")
+    @TestCase("07-01-2017")
+    @TestCase("2018/04/04")
+    @TestCase("2018-01-40")
+    public shouldReturnFalseWhenInvalidDateProvided(providedDate: string) {
+        const result = isValidDate(providedDate);
+
+        Expect(result).toBe(false);
+    }
+
+}
