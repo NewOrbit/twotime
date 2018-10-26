e460035f75975e6ae50781bc9c140a014a996bcb
James Monger <jameskmonger@hotmail.co.uk>
2018-10-26 16:17:48 +0100

    Implement resume

--- src/commands/resume.ts
+++ src/commands/resume.ts
@@ -1,5 +1,29 @@
+import * as inquirer from "inquirer";
 import { ApiProvider } from "../api-provider";
+import { getTodayDate } from "../utils/get-today-date";
+import { getUnfinishedTimeEntries } from "../utils/get-unfinished-time-entries";
+import { HarvestTimeEntry } from "../harvest/api";
+import { getTimeEntryPrompt } from "../utils/get-time-entry-prompt";
+import { log } from "../utils/log";
 
-export const resume = (apiProvider: ApiProvider) => {
-    console.log("resuming");
+export const resume = async (apiProvider: ApiProvider) => {
+    const date = getTodayDate();
+    const harvestApi = apiProvider.getHarvestApi();
+
+    const entries = await harvestApi.getTimeEntries(date);
+
+    const unfinished = getUnfinishedTimeEntries(entries).filter(e => !e.running);
+
+    const prompts = unfinished.map(getTimeEntryPrompt);
+
+    const { timeEntry } = await inquirer.prompt<{ timeEntry: HarvestTimeEntry }>({
+        name: "timeEntry",
+        message: "Which timer would you like to resume?",
+        type: "list",
+        choices: prompts
+    });
+
+    log.info("Resuming Harvest timer");
+    await harvestApi.resumeTimeEntry(timeEntry.id);
+    log.info("> Timer resumed");
 };
