04d37a6ef20a2cc7c6c1196312a2fe79fd9c6848
James Monger <jameskmonger@hotmail.co.uk>
2018-10-26 14:31:49 +0100

    Add extra logs

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,13 +1,14 @@
 import { askStartDetails } from "./prompts/start";
-import { getTodayDate } from "../utils/get-today-date";
 import { createNotes } from "../harvest/notes/create-notes";
 import { createNoteInformation } from "../harvest/notes/create-note-information";
 import { ApiProvider } from "../api-provider";
+import { log } from "../utils/log";
 
 export const start = async (apiProvider: ApiProvider, date: string) => {
     const details = await askStartDetails(apiProvider);
 
     if (details === null) {
+        log.info("No timer started");
         return;
     }
 
@@ -18,5 +19,7 @@ export const start = async (apiProvider: ApiProvider, date: string) => {
 
     const harvestApi = apiProvider.getHarvestApi();
 
+    log.info("Starting Harvest timer...");
     await harvestApi.startTimeEntry(details.projectId, details.taskId, date, notes, details.hours, details.running);
+    log.info("> Timer started");
 };
