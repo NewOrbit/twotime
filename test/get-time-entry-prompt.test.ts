eaefa42ce7af2edc3c8539f1091075452deee000
James Monger <jameskmonger@hotmail.co.uk>
2018-10-29 13:43:40 +0000

    Handle null metadata in getTimeEntryPrompt

--- test/get-time-entry-prompt.test.ts
+++ test/get-time-entry-prompt.test.ts
@@ -6,9 +6,9 @@ import { getTimeEntryPrompt } from "../src/utils/get-time-entry-prompt";
 @TestFixture()
 export class GetTimeEntryPromptTests {
 
-    @TestCase(123, "Foo", 0, EntityType.BUG, "bug #123 (0.00 hours) Foo")
-    @TestCase(456, "Bar", 1.2, EntityType.TASK, "task #456 (1.20 hours) Bar")
-    @TestCase(48337, "Bla bla this is a bug", 1.748, EntityType.BUG, "bug #48337 (1.75 hours) Bla bla this is a bug")
+    @TestCase(123, "Foo", 0, EntityType.BUG, "0.00 hours - Foo (bug #123)")
+    @TestCase(456, "Bar", 1.2, EntityType.TASK, "1.20 hours - Bar (task #456)")
+    @TestCase(48337, "Bla bla this is a bug", 1.748, EntityType.BUG, "1.75 hours - Bla bla this is a bug (bug #48337)")
     public shouldReturnCorrectPrompt(entityId: number, entityName: string, hours: number, entityType: EntityType, expectedPrompt: string) {
         const entry: HarvestTimeEntry = {
             id: 123,
@@ -37,4 +37,47 @@ export class GetTimeEntryPromptTests {
         Expect(result).toEqual(expected);
     }
 
+    @TestCase([ "foo", "barr" ], 1.0, "1.00 hours - foo (no tp entity)")
+    @TestCase([ "a longer line with some more info" ], 1.78, "1.78 hours - a longer line with some more info (no tp entity)")
+    public shouldReturnCorrectPromptForNullMetadata(notes: string[], hours: number, expectedPrompt: string) {
+        const entry: HarvestTimeEntry = {
+            id: 123,
+            metadata: null,
+            hours,
+            notes,
+            created: "2017-06-26T22:32:52Z",
+            running: false
+        };
+
+        const expected = {
+            value: entry,
+            name: expectedPrompt
+        };
+
+        const result = getTimeEntryPrompt(entry);
+
+        Expect(result).toEqual(expected);
+    }
+
+    @TestCase([ ], 1.78, "1.78 hours - no notes provided (no tp entity)")
+    @TestCase([ ], 2.55, "2.55 hours - no notes provided (no tp entity)")
+    public shouldReturnCorrectPromptForNullMetadataAndNoNotes(notes: string[], hours: number, expectedPrompt: string) {
+        const entry: HarvestTimeEntry = {
+            id: 123,
+            metadata: null,
+            hours,
+            notes,
+            created: "2017-06-26T22:32:52Z",
+            running: false
+        };
+
+        const expected = {
+            value: entry,
+            name: expectedPrompt
+        };
+
+        const result = getTimeEntryPrompt(entry);
+
+        Expect(result).toEqual(expected);
+    }
 }
