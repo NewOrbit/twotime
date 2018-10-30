b12c7c709f2225033f9ffc0d6e646c4860442e00
James Monger <jameskmonger@hotmail.co.uk>
2018-10-30 11:01:26 +0000

    Use isPausedAndUnfinished

--- src/commands/resume.ts
+++ src/commands/resume.ts
@@ -4,9 +4,7 @@ import { getTodayDate } from "../utils/get-today-date";
 import { HarvestTimeEntry } from "../harvest/api";
 import { getTimeEntryPrompt } from "../utils/get-time-entry-prompt";
 import { log } from "../utils/log";
-import { isRunningOrUnfinished } from "../utils/is-running-or-unfinished";
-
-const entryIsNotFinished = (entry: HarvestTimeEntry) => entry.metadata === null || entry.metadata.finished === false;
+import { isPausedAndUnfinished } from "../utils/is-paused-and-unfinished";
 
 export const resume = async (apiProvider: ApiProvider) => {
     const date = getTodayDate();
@@ -14,7 +12,7 @@ export const resume = async (apiProvider: ApiProvider) => {
 
     const entries = await harvestApi.getTimeEntries(date);
 
-    const nonRunning = entries.filter(isRunningOrUnfinished);
+    const nonRunning = entries.filter(isPausedAndUnfinished);
 
     if (nonRunning.length === 0) {
         log.info("You have no non-running unfinished timers");
