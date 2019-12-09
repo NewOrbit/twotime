70a5bc33d726aa5f11018c4e291f887bc3129fee
petevb <petevb@neworbit.co.uk>
2019-12-09 16:36:36 +0000

    Handle HTTP 401 and log to console.
    ... rather than some "failed promise" spew.

--- src/utils/get-tp-entity.ts
+++ src/utils/get-tp-entity.ts
@@ -1,4 +1,5 @@
 import { Targetprocess } from "targetprocess-rest-api";
+import { log } from "../utils/log";
 
 export const getTargetprocessEntity = async (api: Targetprocess, id: number) => {
     // tasks are most used, then bugs, then stories - so attempt to find them in that order
@@ -8,8 +9,14 @@ export const getTargetprocessEntity = async (api: Targetprocess, id: number) => 
 
         return task;
     } catch (e) {
-        if (e.statusCode !== 404) {
-            throw e;
+        switch (e.statusCode) {
+            case 401:
+                log.error("TP returned 401: Please check your credentials and run `twotime auth`.");
+                return null; // caller will exit cleanly for null as failure.
+            case 404:
+                break;
+            default:
+                throw e;
         }
 
         try {
