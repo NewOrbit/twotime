c43c1e18f17dc8b6ab2e7c5b153b41257259b369
Tarun Dalwani <tdalwani@neworbit.co.uk>
2020-10-19 13:39:53 +0530

    code cleanup

--- src/commands/finish.ts
+++ src/commands/finish.ts
@@ -6,106 +6,106 @@ import { askFinishDetails, FinishTimerRequest } from "./prompts/finish";
 import { ApiProvider } from "../api-provider";
 
 const stopHarvestTimer = async (harvestApi: HarvestApi, request: FinishTimerRequest, packageVersion: string) => {
-  log.info(`> Updating Harvest`);
+    log.info(`> Updating Harvest`);
 
-  const { timeEntry, tpEntity, timeRemaining } = request;
+    const { timeEntry, tpEntity, timeRemaining } = request;
 
-  if (timeEntry.metadata !== null) {
-    timeEntry.metadata.finished = true;
-    timeEntry.metadata.version = packageVersion;
+    if (timeEntry.metadata !== null) {
+        timeEntry.metadata.finished = true;
+        timeEntry.metadata.version = packageVersion;
 
-    const notes = createNotes(timeEntry.metadata, timeEntry.notes);
+        const notes = createNotes(timeEntry.metadata, timeEntry.notes);
 
-    await harvestApi.updateNotes(timeEntry.id, notes);
-  }
+        await harvestApi.updateNotes(timeEntry.id, notes);
+    }
 
-  // if the timer is running, it needs to be stopped
-  if (timeEntry.running) {
-    await harvestApi.stopTimeEntry(timeEntry.id);
-  }
+    // if the timer is running, it needs to be stopped
+    if (timeEntry.running) {
+        await harvestApi.stopTimeEntry(timeEntry.id);
+    }
 };
 
 const getTargetprocessTimeEntity = async (targetprocessApi: Targetprocess, tpEntity: any) => {
-  // if it's not a bug, we always log the time directly
-  if (tpEntity.ResourceType !== "Bug") {
+    // if it's not a bug, we always log the time directly
+    if (tpEntity.ResourceType !== "Bug") {
+        return tpEntity;
+    }
+
+    const issueTimeTo = await targetprocessApi.getCustomValueForProject(tpEntity.Project.Id, "IssueTime to");
+
+    if (issueTimeTo === "none") {
+        log.info(`Project ${tpEntity.Project.Name} (${tpEntity.Project.Id}) is not configured to log issue time`);
+        return null;
+    }
+
+    if (issueTimeTo === "User story") {
+        log.info(`Project ${tpEntity.Project.Name} (${tpEntity.Project.Id}) is configured to log issue time to the user story`);
+        return tpEntity.UserStory;
+    }
+
+    log.info(`Project ${tpEntity.Project.Name} (${tpEntity.Project.Id}) is configured to log issue time to the issue`);
+
     return tpEntity;
-  }
-
-  const issueTimeTo = await targetprocessApi.getCustomValueForProject(tpEntity.Project.Id, "IssueTime to");
-
-  if (issueTimeTo === "none") {
-    log.info(`Project ${tpEntity.Project.Name} (${tpEntity.Project.Id}) is not configured to log issue time`);
-    return null;
-  }
-
-  if (issueTimeTo === "User story") {
-    log.info(`Project ${tpEntity.Project.Name} (${tpEntity.Project.Id}) is configured to log issue time to the user story`);
-    return tpEntity.UserStory;
-  }
-
-  log.info(`Project ${tpEntity.Project.Name} (${tpEntity.Project.Id}) is configured to log issue time to the issue`);
-
-  return tpEntity;
 };
 
 const getTargetprocessNotes = (request: FinishTimerRequest, entity: any) => {
-  if (request.tpEntity.Id !== entity.Id) {
-    return `time spent on ${request.tpEntity.ResourceType.toLowerCase()} #${request.tpEntity.Id}`;
-  }
+    if (request.tpEntity.Id !== entity.Id) {
+        return `time spent on ${ request.tpEntity.ResourceType.toLowerCase() } #${ request.tpEntity.Id }`;
+    }
 
-  if (request.timeEntry.notes.length > 0) {
-    return request.timeEntry.notes.join("\n");
-  }
+    if (request.timeEntry.notes.length > 0) {
+        return request.timeEntry.notes.join("\n");
+    }
 
-  return "-";
+    return "-";
 };
 
 const updateTargetprocess = async (targetprocessApi: Targetprocess, request: FinishTimerRequest) => {
-  if (request.tpEntity === null) {
-    return;
-  }
+    if (request.tpEntity === null) {
+        return;
+    }
 
-  log.info(`> Updating Targetprocess`);
+    log.info(`> Updating Targetprocess`);
 
-  const timeEntity = await getTargetprocessTimeEntity(targetprocessApi, request.tpEntity);
+    const timeEntity = await getTargetprocessTimeEntity(targetprocessApi, request.tpEntity);
 
-  if (timeEntity === null) {
-    return;
-  }
+    if (timeEntity === null) {
+        return;
+    }
 
-  const notes = getTargetprocessNotes(request, timeEntity);
+    const notes = getTargetprocessNotes(request, timeEntity);
 
-  // Issue #64: If the linked TP entity is a Task *and* its timeRemaining is 0, set the task to 'Done'
-  if (timeEntity.ResourceType === "Task" && request.timeRemaining === 0) {
-    await targetprocessApi.setTaskState(timeEntity.Id, "Done");
-  }
+    // Issue #64: If the linked TP entity is a Task *and* its timeRemaining is 0, set the task to 'Done'
+    if (timeEntity.ResourceType === "Task" && request.timeRemaining === 0) {
+      await targetprocessApi.setTaskState(timeEntity.Id, "Done");
+    }
 
-  return targetprocessApi.addTime(timeEntity.Id, request.timeEntry.hours, request.timeRemaining, new Date(request.timeEntry.created), notes);
+    return targetprocessApi.addTime(timeEntity.Id, request.timeEntry.hours, request.timeRemaining, new Date(request.timeEntry.created), notes);
 };
 
 const getTimerDisplayName = (request: FinishTimerRequest) => {
-  if (request.tpEntity === null) {
-    return `"${request.timeEntry.notes[0] || "no description"}"`;
-  }
+    if (request.tpEntity === null) {
+        return `"${request.timeEntry.notes[0] || "no description"}"`;
+    }
 
-  return `#${request.tpEntity.Id}`;
+    return `#${request.tpEntity.Id}`;
 };
 
 export const finish = async (packageVersion: string, apiProvider: ApiProvider, date: string, all: boolean) => {
-  const finishTimerRequests = await askFinishDetails(apiProvider, date, all);
+    const finishTimerRequests = await askFinishDetails(apiProvider, date, all);
 
-  const harvestApi = apiProvider.getHarvestApi();
-  const targetprocessApi = apiProvider.getTargetprocessApi();
+    const harvestApi = apiProvider.getHarvestApi();
+    const targetprocessApi = apiProvider.getTargetprocessApi();
 
-  for (const request of finishTimerRequests) {
-    log.info(`Finishing timer for ${getTimerDisplayName(request)}`);
+    for (const request of finishTimerRequests) {
+        log.info(`Finishing timer for ${ getTimerDisplayName(request) }`);
 
-    await updateTargetprocess(targetprocessApi, request);
+        await updateTargetprocess(targetprocessApi, request);
 
-    await stopHarvestTimer(harvestApi, request, packageVersion);
+        await stopHarvestTimer(harvestApi, request, packageVersion);
 
-    log.info(`> Timer finished\n`);
-  }
+        log.info(`> Timer finished\n`);
+    }
 
-  log.info(`Finished ${finishTimerRequests.length} timers`);
+    log.info(`Finished ${ finishTimerRequests.length } timers`);
 };
