01beb91d93113b397cf3fbdfe3091c07bf3401a5
Tarun Dalwani <tdalwani@neworbit.co.uk>
2021-02-10 22:16:04 +0530

    start TP task on start

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -16,10 +16,29 @@ export const start = async (packageVersion: string, apiProvider: ApiProvider, da
     const notes = createNotes(noteInformation, [ details.notes ]);
 
     const harvestApi = apiProvider.getHarvestApi();
+    const targetprocessApi = apiProvider.getTargetprocessApi();
 
     log.info("Starting Harvest timer...");
     try {
         await harvestApi.startTimeEntry(details.projectId, details.taskId, date, notes, details.hours, details.running);
+                
+        // Issue #81: If the linked TP entity is a Task *and* its current state is 'Open' or 'Dev ready', set the task to 'In Progress'
+        // Note: It is important to verify current state of Task, otherwise if a 'Done' task with time remaining is moved to 'In Progress',
+        // it may increase the time on a story / feature which the dev / user might not realize as the task has moved to 'In Progress' by this utility
+        // And if the time remaining has not reached 0, the task might not move to 'Done' when the timer is finished        
+        if (details.entity &&
+            details.entity.Id &&
+            details.entity.ResourceType === "Task" &&
+            details.entity.EntityState &&
+            details.entity.EntityState.Name &&
+            (details.entity.EntityState.Name.toUpperCase() === "OPEN" ||
+            details.entity.EntityState.Name.toUpperCase() === "DEV READY") &&
+            details.entity.Project &&
+            details.entity.Project.Process &&
+            details.entity.Project.Process.Id) {
+
+            await targetprocessApi.setTaskState(details.entity.Id, "In Progress", details.entity.Project.Process.Id);
+        }
     } catch (e) {
         log.error(e);
     }
